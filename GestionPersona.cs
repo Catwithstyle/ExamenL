3c1a9671813e3e64c655ce51b0a8e37322ed0166
Ruben <rubenernestovalle@gmail.com>
2024-10-20 20:34:21 -0600

    Primer ejercicio

---
+++ GestionPersona.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_sem_9
+{
+    struct Persona
+    {
+        public string Nombre;
+        public string Direccion;
+        public string Telefono;
+        public int Edad;
+    }
+
+    internal class GestionPersona
+    {
+        private Persona[] personas = new Persona[10];
+        private int contador = 0;
+        public void AgregarPersona(string nombre, string direccion, string telefono, int edad)
+        {
+            if (contador < 10)
+            {
+                personas[contador].Nombre = nombre;
+                personas[contador].Direccion = direccion;
+                personas[contador].Telefono = telefono;
+                personas[contador].Edad = edad;
+                contador++;
+            }
+            else
+            {
+                Console.WriteLine("No se pueden agregar más personas. Capacidad máxima alcanzada.");
+            }
+
+        }
+        public void MostrarNombres()
+        {
+            Console.WriteLine("Lista de nombres:");
+            for (int i = 0; i < contador; i++)
+            {
+                Console.WriteLine(personas[i].Nombre);
+            }
+        }
+        public void MostrarPersonasPorEdad(int edad)
+        {
+            Console.WriteLine($"Personas con {edad} años:");
+            for (int i = 0; i < contador; i++)
+            {
+                if (personas[i].Edad == edad)
+                {
+                    Console.WriteLine($"Nombre: {personas[i].Nombre}, Dirección: {personas[i].Direccion}, Teléfono: {personas[i].Telefono}");
+                }
+            }
+        }
+        public void BuscarPersonaPorNombre(string nombre)
+        {
+            Console.WriteLine($"Personas con el nombre {nombre}:");
+            for (int i = 0; i < contador; i++)
+            {
+                if (personas[i].Nombre.ToLower() == nombre.ToLower())
+                {
+                    Console.WriteLine($"Nombre: {personas[i].Nombre}, Dirección: {personas[i].Direccion}, Teléfono: {personas[i].Telefono}, Edad: {personas[i].Edad}");
+                }
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_sem_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            {
+                GestionPersona gestion = new GestionPersona();
+                bool continuar = true;
+
+                while (continuar)
+                {
+                    Console.WriteLine("Ingrese los datos de la persona:");
+                    Console.Write("Nombre: ");
+                    string nombre = Console.ReadLine();
+                    Console.Write("Dirección: ");
+                    string direccion = Console.ReadLine();
+                    Console.Write("Teléfono: ");
+                    string telefono = Console.ReadLine();
+                    Console.Write("Edad: ");
+                    int edad = int.Parse(Console.ReadLine());
+
+                    gestion.AgregarPersona(nombre, direccion, telefono, edad);
+
+                    Console.WriteLine("¿Desea agregar otra persona? (s/n)");
+                    continuar = Console.ReadLine().ToLower() == "s";
+                }
+
+                int opcion;
+                do
+                {
+                    Console.WriteLine("\nMenú:");
+                    Console.WriteLine("1. Mostrar la lista de todos los nombres.");
+                    Console.WriteLine("2. Mostrar las personas de una cierta edad.");
+                    Console.WriteLine("3. Mostrar las personas que coincidan con un nombre.");
+                    Console.WriteLine("4. Salir.");
+                    Console.Write("Seleccione una opción: ");
+                    opcion = int.Parse(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            gestion.MostrarNombres();
+                            break;
+                        case 2:
+                            Console.Write("Ingrese la edad a buscar: ");
+                            int edadBuscar = int.Parse(Console.ReadLine());
+                            gestion.MostrarPersonasPorEdad(edadBuscar);
+                            break;
+                        case 3:
+                            Console.Write("Ingrese el nombre a buscar: ");
+                            string nombreBuscar = Console.ReadLine();
+                            gestion.BuscarPersonaPorNombre(nombreBuscar);
+                            break;
+                        case 4:
+                            Console.WriteLine("Saliendo del programa...");
+                            break;
+                        default:
+                            Console.WriteLine("Opción no válida.");
+                            break;
+                    }
+                } while (opcion != 4);
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
